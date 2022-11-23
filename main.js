fd1e35d553e4c579fe443b4368a0edc0670e51a5
jacobwanner <106052324+jacobwanner@users.noreply.github.com>
2022-11-22 21:01:10 -0500

    search feature added

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // const locationInput = document.getElementById("locationInput");
 // const searchButton = document.getElementById('searchButton');
-const locationSearch = "22406";
+const locationSearch = prompt('what place would you like to see the weather for? (search by "zipcode" or "city,country")');
 const display = document.querySelector('display');
 const placeDiv = document.getElementById("place")
 const feelsLikeTempDiv = document.getElementById("feelsLike");
@@ -18,7 +18,7 @@ async function getWeather(){
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationSearch}&units=imperial&APPID=501ab2b8a6282d7d36f77e3aa0416c67`, {mode: 'cors'})
   const weatherData = await response.json();
   console.log(weatherData);
-  placeDiv.innerText = `The weather for ${weatherData.name}, ${weatherData.sys.country} is:`;
+  placeDiv.innerText = `The closest station for your search is at: ${weatherData.name}, ${weatherData.sys.country}`;
   feelsLikeTempDiv.innerText = `feels like: ${weatherData.main.feels_like}°F`;
   trueTempDiv.innerText = `true temp: ${weatherData.main.temp}°F`;
   humidityDiv.innerText = `humidity: ${weatherData.main.humidity}%`;
