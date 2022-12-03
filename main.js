d4f18ad335fc9cb9aced26473a958b8e860a27e0
jacobwanner <106052324+jacobwanner@users.noreply.github.com>
2022-12-02 23:50:38 -0500

    input field and search button added

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-// const locationInput = document.getElementById("locationInput");
-// const searchButton = document.getElementById('searchButton');
-const locationSearch = prompt('what place would you like to see the weather for? (search by "zipcode" or "city,country")');
+const locationInput = document.getElementById("locationInput");
+locationInput.value = "Fredericksburg, Virginia"
+const searchButton = document.getElementById('searchButton');
+searchButton.onclick = getWeather;
+
 const display = document.querySelector('display');
 const placeDiv = document.getElementById("place")
 const feelsLikeTempDiv = document.getElementById("feelsLike");
@@ -9,21 +11,67 @@ const humidityDiv = document.getElementById("humidity");
 const cloudIndexDiv = document.getElementById("cloudIndex");
 const windSpeedsDiv = document.getElementById("windSpeeds");
 
-// searchButton.addEventListener("click", () => {
-//   locationSearch = locationInput.value;
-//   return getWeather;
-// });
-
 async function getWeather(){
+  let locationSearch = locationInput.value;
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationSearch}&units=imperial&APPID=501ab2b8a6282d7d36f77e3aa0416c67`, {mode: 'cors'})
   const weatherData = await response.json();
   console.log(weatherData);
-  placeDiv.innerText = `The closest station for your search is at: ${weatherData.name}, ${weatherData.sys.country}`;
+  placeDiv.innerText = `In ${locationSearch} the weather is:`;
   feelsLikeTempDiv.innerText = `feels like: ${weatherData.main.feels_like}°F`;
   trueTempDiv.innerText = `true temp: ${weatherData.main.temp}°F`;
   humidityDiv.innerText = `humidity: ${weatherData.main.humidity}%`;
   cloudIndexDiv.innerText = `weather: ${weatherData.weather[0].description}`;
   windSpeedsDiv.innerText = `wind speeds: ${weatherData.wind.speed} mph`
+  locationInput.value = "";
 }
 
-getWeather();
+// Alabama 	AL
+// Alaska 	AK
+// Arizona 	AZ	
+// Arkansas 	AR
+// California 	CA
+// Colorado 	CO
+// Connecticut 	CT
+// Delaware 	DE
+// Florida 	FL
+// Georgia 	GA
+// Hawaii 	HI
+// Idaho 	ID
+// Illinois 	IL
+// Indiana 	IN
+// Iowa 	IA
+// Kansas 	KS
+// Kentucky 	KY
+// Louisiana 	LA
+// Maine 	ME
+// Maryland 	MD
+// Massachusetts 	MA
+// Michigan 	MI
+// Minnesota 	MN
+// Mississippi 	MS
+// Missouri 	MO
+// Montana 	MT
+// Nebraska 	NE
+// Nevada 	NV
+// New Hampshire 	NH
+// New Jersey 	NJ
+// New Mexico 	NM
+// New York 	NY
+// North Carolina 	NC
+// North Dakota 	ND
+// Ohio 	OH
+// Oklahoma 	OK
+// Oregon 	OR
+// Pennsylvania 	PA
+// Rhode Island 	RI
+// South Carolina 	SC
+// South Dakota 	SD
+// Tennessee 	TN
+// Texas 	TX
+// Utah 	UT
+// Vermont 	VT
+// Virginia 	VA
+// Washington 	WA
+// West Virginia 	WV
+// Wisconsin 	WI
+// Wyoming 	WY
